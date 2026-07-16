24378492d39d50a606403dc8476ef068becf3999
Miasoled <lissethgetial@gmail.com>
2026-07-16 10:52:38 -0500

    Estilo con Material UI

--- src/components/personaje-card/personaje-card.jsx
+++ src/components/personaje-card/personaje-card.jsx
@@ -1,17 +1,30 @@
 import PropTypes from "prop-types";
-import styles from "./personaje-card.module.css";
+/*import styles from "./personaje-card.module.css";*/
+//Para Card
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export const PersonajeCard = ({ nombre, especie, url }) => {
   return (
-    <div className={styles.personajeCard}>
-      <div className={styles.imageWrapper}>
-        <img src={url} alt={nombre} className={styles.imagen} />
-        <div className={styles.especieBadge}>{especie}</div>
-      </div>
-      <div className={styles.info}>
-        <h3 className={styles.nombre}>{nombre}</h3>
-      </div>
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia sx={{ height: 140 }} image={url} title={nombre} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {nombre}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {especie}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 };
 
