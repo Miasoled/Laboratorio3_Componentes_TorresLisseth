192ef0c51645ff056129e12a78a5e3b4fe7383c8
Miasoled <lissethgetial@gmail.com>
2026-07-16 10:33:45 -0500

    Implementacion de PWA

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "html"],
-      exclude: ["node_modules/", "src/main.jsx"],
-    },
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "APP PWA",
+        name: "Mi Aplicación PWA",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "black",
+        background_color: "white",
+      },
+    }),
+  ],
 });
